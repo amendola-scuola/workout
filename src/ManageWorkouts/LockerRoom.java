8bcaa17c006c2adbd1377eec756b69dfc61fcd31
Francesco Amendola <francesco.amendola@risikamente.it>
2023-10-22 23:39:48 +0200

    Aggiunta metodo synchronized e correzzioni varie

--- src/ManageWorkouts/LockerRoom.java
+++ src/ManageWorkouts/LockerRoom.java
@@ -7,31 +7,27 @@ public class LockerRoom {
     }
 
     public void usaSpogliatoio(String nomeTeam) {
-        while(!spogliatoioLibero) {
-            System.out.println("Spogliatoio occupato! Il team: "
-                    + nomeTeam + " sta aspettando che si liberi...");
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                System.err
-                        .println("Errore wait()\n" + e.getStackTrace());
+        synchronized(this) {
+            while (!spogliatoioLibero) {
+                System.out.println("Spogliatoio occupato! Il team: "
+                        + nomeTeam + " sta aspettando che si liberi...");
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.err.println("Errore wait()\n" + e.getStackTrace());
+                }
             }
-        }
 
-        spogliatoioLibero = false;
-        try {
-            System.out
-                    .println("Inizio ad utilizzare lo spogliatoio!"
-                            + nomeTeam);
-            Thread.currentThread().sleep(3000);
-            System.out
-                    .println("Ho finito di utilizzare lo spogliatoio!"
-                            + nomeTeam);
-        } catch (InterruptedException e) {
-            System.err
-                    .println("Errore sleep()\n" + e.getStackTrace());
+            spogliatoioLibero = false;
+            try {
+                System.out.println("La squadra " + nomeTeam + " ha iniziato ad utilizzare lo spogliatoio!");
+                wait(3000);
+                System.out.println("La squadra " + nomeTeam + " ha finito di utilizzare lo spogliatoio!");
+            } catch (InterruptedException e) {
+                System.err.println("Errore sleep()\n" + e.getStackTrace());
+            }
+            spogliatoioLibero = true;
+            notify();
         }
-        spogliatoioLibero = true;
-        notify();
     }
 }
